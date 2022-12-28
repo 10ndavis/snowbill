5c7bd0c56b558cf6482ff8838d67eba928c8feb4
Nathan Davis <10ndavis10@gmail.com>
2022-12-28 14:27:15 -0600

    added delete button to add/edit debt drawer

--- lib/widgets/add_debt_sheet.dart
+++ lib/widgets/add_debt_sheet.dart
@@ -78,153 +78,174 @@ class _AddDebtSheetState extends State<AddDebtSheet> {
         color: Theme.of(context).backgroundColor,
         child: SafeArea(
           child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                TextField(
-                  cursorColor: Theme.of(context).colorScheme.secondary,
-                  controller: name,
-                  style: TextStyle(color: Theme.of(context).colorScheme.secondary),
-                  keyboardType: TextInputType.name,
-                  keyboardAppearance: Brightness.dark,
-                  textInputAction: TextInputAction.next,
-                  decoration: InputDecoration(
-                    label: const Text('Name'),
-                    hintStyle: const TextStyle(
-                      color: Colors.white30,
+            child: Stack(
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    TextField(
+                      cursorColor: Theme.of(context).colorScheme.secondary,
+                      controller: name,
+                      style: TextStyle(color: Theme.of(context).colorScheme.secondary),
+                      keyboardType: TextInputType.name,
+                      keyboardAppearance: Brightness.dark,
+                      textInputAction: TextInputAction.next,
+                      decoration: InputDecoration(
+                        label: const Text('Name'),
+                        hintStyle: const TextStyle(
+                          color: Colors.white30,
+                        ),
+                        labelStyle: TextStyle(
+                          color: Theme.of(context).colorScheme.secondary,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                        border: InputBorder.none,
+                      ),
                     ),
-                    labelStyle: TextStyle(
-                      color: Theme.of(context).colorScheme.secondary,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
+                    TextField(
+                      controller: monthlyPayment,
+                      style: TextStyle(color: Theme.of(context).colorScheme.secondary),
+                      keyboardType: const TextInputType.numberWithOptions(decimal: true),
+                      keyboardAppearance: Brightness.dark,
+                      textInputAction: TextInputAction.next,
+                      cursorColor: Theme.of(context).colorScheme.secondary,
+                      decoration: InputDecoration(
+                        prefixText: '\$',
+                        prefixStyle: TextStyle(
+                          color: Theme.of(context).colorScheme.secondary,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                        label: const Text('Monthly Payment with Interest'),
+                        labelStyle: TextStyle(
+                          color: Theme.of(context).colorScheme.secondary,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                        hintStyle: const TextStyle(
+                          color: Colors.white30,
+                        ),
+                        border: InputBorder.none,
+                      ),
                     ),
-                    border: InputBorder.none,
-                  ),
-                ),
-                TextField(
-                  controller: monthlyPayment,
-                  style: TextStyle(color: Theme.of(context).colorScheme.secondary),
-                  keyboardType: const TextInputType.numberWithOptions(decimal: true),
-                  keyboardAppearance: Brightness.dark,
-                  textInputAction: TextInputAction.next,
-                  cursorColor: Theme.of(context).colorScheme.secondary,
-                  decoration: InputDecoration(
-                    prefixText: '\$',
-                    prefixStyle: TextStyle(
-                      color: Theme.of(context).colorScheme.secondary,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
+                    TextField(
+                      cursorColor: Theme.of(context).colorScheme.secondary,
+                      controller: remainingBalance,
+                      style: TextStyle(color: Theme.of(context).colorScheme.secondary),
+                      keyboardType: const TextInputType.numberWithOptions(decimal: true),
+                      keyboardAppearance: Brightness.dark,
+                      textInputAction: TextInputAction.done,
+                      decoration: InputDecoration(
+                        prefixText: '\$',
+                        prefixStyle: TextStyle(
+                          color: Theme.of(context).colorScheme.secondary,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                        label: const Text('Balance Remaining with Interest'),
+                        labelStyle: TextStyle(
+                          color: Theme.of(context).colorScheme.secondary,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                        hintStyle: const TextStyle(
+                          color: Colors.white30,
+                        ),
+                        border: InputBorder.none,
+                      ),
                     ),
-                    label: const Text('Monthly Payment with Interest'),
-                    labelStyle: TextStyle(
-                      color: Theme.of(context).colorScheme.secondary,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
+                    TextField(
+                      cursorColor: Theme.of(context).colorScheme.secondary,
+                      controller: interestRate,
+                      style: TextStyle(color: Theme.of(context).colorScheme.secondary),
+                      keyboardType: const TextInputType.numberWithOptions(decimal: true),
+                      keyboardAppearance: Brightness.dark,
+                      textInputAction: TextInputAction.done,
+                      decoration: InputDecoration(
+                        label: const Text('Interest Rate'),
+                        suffixText: '%',
+                        suffixStyle: TextStyle(
+                          color: Theme.of(context).colorScheme.secondary,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                        labelStyle: TextStyle(
+                          color: Theme.of(context).colorScheme.secondary,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                        hintStyle: const TextStyle(
+                          color: Colors.white30,
+                        ),
+                        border: InputBorder.none,
+                      ),
                     ),
-                    hintStyle: const TextStyle(
-                      color: Colors.white30,
-                    ),
-                    border: InputBorder.none,
-                  ),
-                ),
-                TextField(
-                  cursorColor: Theme.of(context).colorScheme.secondary,
-                  controller: remainingBalance,
-                  style: TextStyle(color: Theme.of(context).colorScheme.secondary),
-                  keyboardType: const TextInputType.numberWithOptions(decimal: true),
-                  keyboardAppearance: Brightness.dark,
-                  textInputAction: TextInputAction.done,
-                  decoration: InputDecoration(
-                    prefixText: '\$',
-                    prefixStyle: TextStyle(
-                      color: Theme.of(context).colorScheme.secondary,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
-                    label: const Text('Balance Remaining with Interest'),
-                    labelStyle: TextStyle(
-                      color: Theme.of(context).colorScheme.secondary,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
-                    hintStyle: const TextStyle(
-                      color: Colors.white30,
-                    ),
-                    border: InputBorder.none,
-                  ),
-                ),
-                TextField(
-                  cursorColor: Theme.of(context).colorScheme.secondary,
-                  controller: interestRate,
-                  style: TextStyle(color: Theme.of(context).colorScheme.secondary),
-                  keyboardType: const TextInputType.numberWithOptions(decimal: true),
-                  keyboardAppearance: Brightness.dark,
-                  textInputAction: TextInputAction.done,
-                  decoration: InputDecoration(
-                    label: const Text('Interest Rate'),
-                    suffixText: '%',
-                    suffixStyle: TextStyle(
-                      color: Theme.of(context).colorScheme.secondary,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
-                    labelStyle: TextStyle(
-                      color: Theme.of(context).colorScheme.secondary,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
-                    hintStyle: const TextStyle(
-                      color: Colors.white30,
-                    ),
-                    border: InputBorder.none,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 24),
-                  child: AnimatedSwitcher(
-                    duration: const Duration(milliseconds: 300),
-                    child: formComplete
-                        ? SizedBox(
-                            child: ElevatedButton(
-                              child: widget.container != null ? const Text('Update') : const Text('Add to Snowball'),
-                              onPressed: () {
-                                if (widget.container != null) {
-                                  Provider.of<SnowballProvider>(context, listen: false).updateDebt(
-                                    widget.container!.debt,
-                                    Debt(
-                                      monthlyPayment: double.parse(monthlyPayment.text),
-                                      name: name.text,
-                                      remainingBalance: double.parse(remainingBalance.text),
-                                      createdAt: widget.container!.debt.createdAt,
-                                      interestRate: double.parse(interestRate.text),
-                                    ),
-                                  );
-                                } else {
-                                  Provider.of<SnowballProvider>(context, listen: false).addDebt(
-                                    Debt(
-                                      monthlyPayment: double.parse(monthlyPayment.text),
-                                      name: name.text,
-                                      remainingBalance: double.parse(remainingBalance.text),
-                                      createdAt: DateTime.now(),
-                                      interestRate: double.parse(interestRate.text),
-                                    ),
-                                  );
-                                }
+                    Padding(
+                      padding: const EdgeInsets.only(top: 24),
+                      child: AnimatedSwitcher(
+                        duration: const Duration(milliseconds: 300),
+                        child: formComplete
+                            ? SizedBox(
+                                child: ElevatedButton(
+                                  child:
+                                      widget.container != null ? const Text('Update') : const Text('Add to Snowball'),
+                                  onPressed: () {
+                                    if (widget.container != null) {
+                                      Provider.of<SnowballProvider>(context, listen: false).updateDebt(
+                                        widget.container!.debt,
+                                        Debt(
+                                          monthlyPayment: double.parse(monthlyPayment.text),
+                                          name: name.text,
+                                          remainingBalance: double.parse(remainingBalance.text),
+                                          createdAt: widget.container!.debt.createdAt,
+                                          interestRate: double.parse(interestRate.text),
+                                        ),
+                                      );
+                                    } else {
+                                      Provider.of<SnowballProvider>(context, listen: false).addDebt(
+                                        Debt(
+                                          monthlyPayment: double.parse(monthlyPayment.text),
+                                          name: name.text,
+                                          remainingBalance: double.parse(remainingBalance.text),
+                                          createdAt: DateTime.now(),
+                                          interestRate: double.parse(interestRate.text),
+                                        ),
+                                      );
+                                    }
 
-                                Navigator.pop(context);
-                              },
-                            ),
-                          )
-                        : Opacity(
-                            opacity: 0,
-                            child: ElevatedButton(
-                              child: const Text('Close BottomSheet'),
-                              onPressed: () {},
-                            ),
-                          ),
-                  ),
+                                    Navigator.pop(context);
+                                  },
+                                ),
+                              )
+                            : Opacity(
+                                opacity: 0,
+                                child: ElevatedButton(
+                                  child: const Text('Close BottomSheet'),
+                                  onPressed: () {},
+                                ),
+                              ),
+                      ),
+                    ),
+                  ],
                 ),
+                if (widget.container != null)
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                          Provider.of<SnowballProvider>(context, listen: false).deleteDebt(widget.container!.debt);
+                        },
+                        icon: Icon(
+                          Icons.delete,
+                          color: Colors.redAccent,
+                        ),
+                      ),
+                    ],
+                  ),
               ],
             ),
           ),
